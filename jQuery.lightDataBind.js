74a05cd990951054a1646a740fa2ba35f1334889
waterwu <ms12+waterwhite@albion.edu>
2013-07-24 16:14:31 +0800

    Update jQuery.lightDataBind.js

--- jQuery.lightDataBind.js
+++ jQuery.lightDataBind.js
@@ -1,37 +1,47 @@
 (function($){
-	$.fn.ldatabind = $.fn.ldata = function(data){
-		var that = this;
+	$.fn.ldatabind = $.fn.ldata = function(data,binds){
+		var that = this,
+			$el = $(this);
+		binds = $.extend($el.data("ldata-bind")||{},binds||{});
+		$el.data("ldata-bind",binds);
 		var isVal = function(value){
 			return value||value==""||value==0;
 		};
 		var attrs = {
+			"data-func":function(dom,value,key,reverse){
+				var f = binds[key];
+				if(key&&f){ 
+					if(!reverse)f(dom,data); 
+					else { return f(dom); }
+				} else { console.warn("No func specified: ",key); }
+			},
 			"data-text":function(dom,value){ return isVal(value)?dom.text(value):dom.text(); },
 			"data-html":function(dom,value){ return isVal(value)?dom.html(value):dom.html(); },
 			"data-src":function(dom,value){ return isVal(value)?dom.attr("src",value):dom.attr("src"); },
 			"data-href":function(dom,value){ return isVal(value)?dom.attr("href",value):dom.attr("href"); },
 			"data-value":function(dom,value){ return isVal(value)?dom.val(value):dom.val(); }
 		};
-		if(!data){
+		if(!data){ // dom to obj
 			data = {};
-			$.each(attrs,function(attr){
-				var bindFunc = this;
+			$.each(attrs,function(attr,bindFunc){
 				$("["+attr+"]",that).each(function(){
 					var d = $(this);
 					var key = d.attr(attr);
-					data[key] = bindFunc(d);
+					var val = bindFunc(d,null,key,true);
+					if(isVal(val))data[key] = val;
 				});
 			});
 			return data;
-		}else {
-			$.each(attrs,function(attr){
-				var bindFunc = this;
+		}else { // obj to dom
+			$.each(attrs,function(attr,bindFunc){
 				$("["+attr+"]",that).each(function(){
 					var d = $(this);
 					var key = d.attr(attr);
 					var value = data[key];
-					bindFunc(d,value);
+					bindFunc(d,value,key);
 				});
 			});
 		}
+		return $el;
 	};
 })(jQuery);
